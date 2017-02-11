5385b25762e38da366764f34a052783f8d293464
DarrenPoole <dazpoole@hotmail.co.uk>
2017-02-11 11:28:39 +0000

    Added Models folder with ComicBook Entity Model. Added the Context class which inherits from DbContext and contains a DbSet of type ComicBook entity model.

---
+++ src/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBookGalleryModel.Models;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
