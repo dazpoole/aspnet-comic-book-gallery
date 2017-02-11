b3248c929d4c51eaea3c8a7a1a576ca55dfd6e4d
DarrenPoole <dazpoole@hotmail.co.uk>
2017-02-11 16:24:58 +0000

    Looked at Data Annotations and Fluent API for our model entities.

--- src/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,12 @@ namespace ComicBookGalleryModel
         }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<ComicBook>().Property(cb => cb.AverageRating).HasPrecision(5, 2);
+        }
     }
 }
--- src/ComicBookGalleryModel/Models/Role.cs
+++ src/ComicBookGalleryModel/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace ComicBookGalleryModel.Models
     {
         public int Id { get; set; }
 
+        [Required, StringLength(50)]
         public string Name { get; set; }
 
     }
