fe8b0f922f57ad0cfa49a130ab513f3af652fa51
DarrenPoole <dazpoole@hotmail.co.uk>
2017-02-11 19:13:18 +0000

    Updated Program.cs testing Queries.

--- src/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ComicBookGalleryModel.Models;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Xml.Serialization.Configuration;
 
 namespace ComicBookGalleryModel
@@ -15,6 +16,22 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
+
+                //var comicBookId = 1;
+
+                //var comicBook1 = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .SingleOrDefault(cb => cb.Id == comicBookId);
+
+                //Debug.WriteLine("Changing the Description property value.");
+                //comicBook1.Description = "New value!";
+
+                //var comicBook2 = context.ComicBooks
+                //    .SingleOrDefault(cb => cb.Id == comicBookId);
+
+
 
                 var comicBooks = context.ComicBooks
                     .Include(cb => cb.Series)
